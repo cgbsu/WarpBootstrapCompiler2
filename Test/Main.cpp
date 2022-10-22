48d7e9b7699946fa8cd5360d04ae390c0ac194e3
cgbsu@github.com <cgreeley@student.bridgew.edu>
2022-10-22 04:38:06 +0000

    Modules dont work :(

--- Test/Main.cpp
+++ Test/Main.cpp
@@ -1,3 +1,6 @@
+import Warp.Utilities;
+
+constexpr static const auto minimum = [](auto left, auto right) { return left < right; };
 
 int main(int argc, char* argv[]) {
 	return 0;
--- Test/TreeSort.cpp
+++ Test/TreeSort.cpp
@@ -1,6 +1,6 @@
-import Warp.Utilities;
+//import Warp.Utilities:Strings;
 
-using namespace Warp::Utilities;
+//using namespace Warp::Utilities;
 
 //template<auto... CompileTimeValueParameterConstants>
 //constexpr auto toRuntimeValues(Sequence<CompileTimeValueParameterConstants...>) 
@@ -11,7 +11,7 @@ using namespace Warp::Utilities;
 //		};
 //}
 	
-constexpr static const auto minimum = [](auto left, auto right) { return left < right; };
+//constexpr static const auto minimum = [](auto left, auto right) { return left < right; };
 
 
 //template<auto... InOrderParameterConstants, auto... JumbledParameterConstants>
@@ -23,22 +23,22 @@ constexpr static const auto minimum = [](auto left, auto right) { return left < 
 //
 //}
 
-TEST_CASE(
-		"CompileTimeTreeSort sorts an unordered list of unique elements", 
-		"[compile-time-tree-sort]"
-	)
-{
-	{
-		using X = decltype(treeSortMakeTree<minimum, ConstantBinaryTreeRootType>(
-				std::index_sequence<14, 5, 82, 1, 10, 2, 6, 18, 17, 3, 30, 16>()
-			));
-		std::cout << X::toString() << "\n";
-		constexpr auto result = treeSort<minimum>(
-				std::index_sequence<14, 5, 82, 1, 10, 2, 6, 18, 17, 3, 30, 16>()
-			);
-		std::cout << decltype(result)::toString() << "\n";
-		constexpr std::array expected{1u, 2u, 3u, 5u, 6u, 10u, 14u, 16u, 17u, 18u, 30u, 82u};
-		//REQUIRE(toRuntimeValues(result) == expected);
-	}
-}
-
+//TEST_CASE(
+//		"CompileTimeTreeSort sorts an unordered list of unique elements", 
+//		"[compile-time-tree-sort]"
+//	)
+//{
+//	{
+//		using X = decltype(treeSortMakeTree<minimum, ConstantBinaryTreeRootType>(
+//				std::index_sequence<14, 5, 82, 1, 10, 2, 6, 18, 17, 3, 30, 16>()
+//			));
+//		std::cout << X::toString() << "\n";
+//		constexpr auto result = treeSort<minimum>(
+//				std::index_sequence<14, 5, 82, 1, 10, 2, 6, 18, 17, 3, 30, 16>()
+//			);
+//		std::cout << decltype(result)::toString() << "\n";
+//		constexpr std::array expected{1u, 2u, 3u, 5u, 6u, 10u, 14u, 16u, 17u, 18u, 30u, 82u};
+//		//REQUIRE(toRuntimeValues(result) == expected);
+//	}
+//}
+//
