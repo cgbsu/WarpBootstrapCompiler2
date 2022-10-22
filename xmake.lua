fdc304e1e762f777f56580403c45c48d16df5cf9
cgbsu@github.com <cgreeley@student.bridgew.edu>
2022-10-22 20:28:44 +0000

    Modules mostly working.

--- xmake.lua
+++ xmake.lua
@@ -1,7 +1,7 @@
 set_languages("c++23")
 
--- add_requires("conan::ctpg/1.3.7", {alias = "ctpg"})
--- add_requires("conan::crc_cpp/1.1.0", {alias = "crc_cpp"})
+add_requires("conan::ctpg/1.3.7", {alias = "ctpg"})
+add_requires("conan::crc_cpp/1.1.0", {alias = "crc_cpp"})
 add_requires("conan::catch2/3.0.1", {alias = "catch2"})
 
 --target("Warp.Common")
@@ -26,16 +26,17 @@ add_requires("conan::catch2/3.0.1", {alias = "catch2"})
 
 target("test")
 	set_kind("binary")
+	add_mflags("-freport-bug", "-E")
 	add_files(
-	"Source/Utilities/*.mpp", 
-	"Source/*.mpp", 
-	--"Source/Common.mpp", 
-	"Test/*.cpp")
+			"Source/Utilities/*.mpp", 
+			"Source/*.mpp", 
+			--"Source/Common.mpp", 
+			"Test/*.cpp"
+	)
 	add_packages("ctpg", "catch2", "crc_cpp")
 	--add_deps("Warp.Common");
 	add_includedirs("Include")
 	add_includedirs("Include/ThirdParty/constexpr-random/include")
-	add_includedirs("Include/ThirdParty/ctpg/include")
 	set_policy("build.c++.modules", true)
 target_end()
 
