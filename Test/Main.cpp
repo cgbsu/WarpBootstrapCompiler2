b067218ec2d4fa96d1b8de03fca326b3d906f7ce
cgbsu@github.com <cgreeley@student.bridgew.edu>
2022-10-22 20:48:20 +0000

    AAA

--- Test/Main.cpp
+++ Test/Main.cpp
@@ -1,10 +1,23 @@
-import <iostream>;
+import Warp.Common;
+import Warp.Utilities;
+
+using namespace Warp::Utilities;
+
 //#define CATCH_CONFIG_MAIN
 //#include <catch2/catch_session.hpp>
 
+constexpr static const auto minimum = [](auto left, auto right) { return left < right; };
+
 int main(int argc, char* argv[]) {
-	std::cout << "Hello\n";
-	return 0;
+{
+	using X = decltype(treeSortMakeTree<minimum, ConstantBinaryTreeRootType>(
+			std::index_sequence<14, 5, 82, 1, 10, 2, 6, 18, 17, 3, 30, 16>()
+		));
+	std::cout << X::toString() << "\n";
+	constexpr auto result = treeSort<minimum>(
+			std::index_sequence<14, 5, 82, 1, 10, 2, 6, 18, 17, 3, 30, 16>()
+		);
+	std::cout << decltype(result)::toString() << "\n";
 }
 
 	//  // your setup ...
