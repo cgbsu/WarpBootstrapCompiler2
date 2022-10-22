c2635b36cc1d1f64fd703a3a27bcfa84b65dbd85
cgbsu@github.com <cgreeley@student.bridgew.edu>
2022-10-22 03:37:31 +0000

    Sort of working, but I think I have to abandon catch2

--- Test/Main.cpp
+++ Test/Main.cpp
@@ -1,17 +1,5 @@
-#define CATCH_CONFIG_MAIN
-#include <catch2/catch_session.hpp>
 
-//int main(int argc, char* argv[]) {
-//	std::cout << "Hello\n";
-//	return 0;
-//}
-
-	//  // your setup ...
-	//
-	//  int result = Catch::Session().run( argc, argv );
-	//
-	//  // your clean-up...
-	//
-	//  return result;
-//}
+int main(int argc, char* argv[]) {
+	return 0;
+}
 
